f87077f3cdbcef05988086b9cc5d9f542a0faa16
Hesam_Farjad <your_email@example.com>
2025-05-07 12:58:38 +0200

    strip in API and Client checkout

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5000/api/',
+  apiUrl: 'https://localhost:5001/api/',
+  stripePublicKey:
+    'pk_test_51RLN94PT9gkdVaMpJt8B2x4Esrfo38VvaDhqU5QZ5H4sZVJ1e8fk45MqDTjYDZRmqJaZksORyVVlx5YGxJX7TifW00isAG3NDD',
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'api/',
+  stripePublicKey:
+    'pk_test_51RLN94PT9gkdVaMpJt8B2x4Esrfo38VvaDhqU5QZ5H4sZVJ1e8fk45MqDTjYDZRmqJaZksORyVVlx5YGxJX7TifW00isAG3NDD',
 };
