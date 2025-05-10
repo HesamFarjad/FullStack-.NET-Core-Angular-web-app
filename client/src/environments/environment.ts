9ddcace911b66071f61774847d121ce9b845ba32
Hesam_Farjad <your_email@example.com>
2025-05-10 23:25:25 +0200

    Client side order , Webhooks, SignalR has been completed in section eighteen

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'api/',
+  hubUrl: 'hub/notifications',
   stripePublicKey:
     'pk_test_51RLN94PT9gkdVaMpJt8B2x4Esrfo38VvaDhqU5QZ5H4sZVJ1e8fk45MqDTjYDZRmqJaZksORyVVlx5YGxJX7TifW00isAG3NDD',
 };
